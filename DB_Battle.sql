643dce1d6f2468e0744b841508a4196f150de0a1
EvanMlj <120018761+EvanMlj@users.noreply.github.com>
2023-08-03 09:43:20 +0200

    feat: Base de donnée DB_Battle.sql sur root

---
+++ DB_Battle.sql
@@ -0,0 +1,30 @@
+-- --------------------------------------------------------
+-- Hôte:                         127.0.0.1
+-- Version du serveur:           8.0.30 - MySQL Community Server - GPL
+-- SE du serveur:                Win64
+-- HeidiSQL Version:             12.1.0.6537
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+-- Listage des données de la table battle.combat : ~1 rows (environ)
+INSERT INTO `combat` (`id_combat`, `id_p1`, `id_p2`, `id_win`, `log_combat`) VALUES
+	(1, 1, 2, 1, '');
+
+-- Listage des données de la table battle.personnage : ~2 rows (environ)
+INSERT INTO `personnage` (`id_personnage`, `name`, `create_at_date`, `attaque`, `mana`, `sante`) VALUES
+	(1, 'test', '2023-08-02', 100, 100, 100),
+	(2, 'test2', '2023-08-02', 100, 100, 100);
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
